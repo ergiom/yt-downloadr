4ac3d2d1279903dfc851c6ec6492f7ecf19b200a
ergiom <ergiom1@gmail.com>
2022-11-06 13:55:40 +0100

    updated config

--- yt_downloadr/__init__.py
+++ yt_downloadr/__init__.py
@@ -2,5 +2,10 @@
 
 from flask import Flask
 app = Flask(__name__)
+app.config.from_prefixed_env()
+app.config['WTF_CSRF_ENABLED'] = False
+
+if not app.secret_key:
+    raise RuntimeError("secret key not set")
 
 import yt_downloadr.routes

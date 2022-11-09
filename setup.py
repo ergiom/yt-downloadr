584cc672e7f6d6ab53e8e26b2ac8e5bdb3d89a86
ergiom <ergiom1@gmail.com>
2022-11-09 20:15:57 +0100

    added src dir

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 '''setup for yt_downloadr'''
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='yt_downloadr',
-    packages=['yt_downloadr'],
+    version='0.1.0',
+    author="Pawel Osuch",
+    description="Simple yt-dlp based webapp",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
